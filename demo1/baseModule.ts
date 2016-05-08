6ff491229d5bb6a167522affc668ea71ef01a7b6
weikang wei <weikang wei>
2016-05-08 17:40:40 +0800

    add baseModule

---
+++ demo1/baseModule.ts
@@ -0,0 +1,11 @@
+﻿export class baseModule {
+    constructor(protected opts: { app: any }) {
+        this.opts = opts;
+    }
+    addView(name: string, views: {}) {
+        this.opts.app.config(name,views);
+    }
+}
+export interface creatView{
+    creatView();
+}
